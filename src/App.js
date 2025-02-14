dc777b78cec5b6507b738897d083230a2e83ade8
Zubair Borkala <zborkala@gmail.com>
2025-02-14 11:37:00 +0530

    Used map for displaying multiple students

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Student from "./components/Student"
 
 function App () {
+  const students = [
+    {
+      name: 'Hashir',
+      age: 23,
+      phone: 9876563865,
+      course: 'Ethical Hacking'
+    },
+    {
+      name: 'Ahmad',
+      age: 22,
+      phone: 56786567666,
+      course: 'Graphic Designing'
+    },
+    {
+      name: 'Muneer',
+      age: 19,
+      phone: 87656766553,
+      course: 'Web Development'
+    },
+  ]
   return (
     <div className='container'>
-      <Student name='Hashir' age='23' phone='7654578353' course='Ethical Hacking' />
-      <Student name='Ahmad' age='22' phone='98764846343' course='Full Stack Development' />
-      <Student name='Abdulla' age='34' phone='6584757465' course='Graphic Designing' />
+      {students.map((student, index) => {
+        return <Student key={index} name={student.name} phone={student.phone} age={student.age} course={student.course} />
+      })}
+      {/* <Student name='Hashir' age='23' phone='7654578353' course='Ethical Hacking' /> */}
     </div>
   )
 }
